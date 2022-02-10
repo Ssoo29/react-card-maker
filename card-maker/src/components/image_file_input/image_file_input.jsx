1aab209c0db7f2166032e17de55906e97a946dd6
김성수 <fereverv@openbase.co.kr>
2022-02-10 11:22:04 +0900

    add loading spinner

--- card-maker/src/components/image_file_input/image_file_input.jsx
+++ card-maker/src/components/image_file_input/image_file_input.jsx
@@ -1,7 +1,8 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styles from "./image_file_input.module.css";
 
 const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
+  const [loading, setLoading] = useState(false);
   const inputRef = useRef();
   const onButtonClick = event => {
     event.preventDefault();
@@ -9,17 +10,20 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
   };
 
   const onChange = async event => {
+    setLoading(true);
     const uploaded = await imageUploader.upload(event.target.files[0]);
     onFileChange({
       name: uploaded.original_filename,
       url: uploaded.url
     })
+    setLoading(false);
   }
 
   return (
     <div className={styles.container}>
       <input className={styles.input} ref={inputRef} type="file" accept="image/*" name="file" onChange={onChange} />
-      <button className={styles.button} onClick={onButtonClick}>{name || 'No file'}</button>
+      {!loading && (<button className={`${styles.button} ${name ? styles.pink : styles.grey}`} onClick={onButtonClick}>{name || 'No file'}</button>)}
+      {loading && <div className={styles.loading}></div>}
     </div>
   );
 };
